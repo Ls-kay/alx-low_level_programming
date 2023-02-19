ac9a09b866a4ec7dee0be123a478e9fe9ef9ec78
Lebohang Msase <msaselebohang@gmail.com>
2023-02-19 04:37:16 -0800

    A program that makes choices

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
  * Return - Always 0 indicates success
  *
  */
+
 /* betty style doc for function main goes there */
 
 int main(void)
@@ -21,15 +22,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive ", n);
+		printf("%d  is positive", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative ", n);
+		printf("%d  is negative", n);
 	}
 	else
 	{
-		printf("%d is zero ", n);
+		printf("%d  is zero", n);
 	}
 
 	return (0);
