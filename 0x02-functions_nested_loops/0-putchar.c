3013cc251fd7b2cf1090a08984ca7ea6e77c3967
Lebohang Msase <msaselebohang@gmail.com>
2023-02-22 19:05:57 -0800

    Printing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,21 @@
 
 int main(void)
 {
-	_putchar("_")
+	char *c = "_putchar";
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	/*_putchar("_")
 	_putchar("p");
 	_putchar("u");
 	_putchar("t");
 	_putchar("c");
 	_putchar("h");
 	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar("r");*/
+	_putchar('\n');
+
 	return (0);
 }
