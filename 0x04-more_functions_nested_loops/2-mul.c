305a99bca98115d2a23b7b8db90182341e0ce27e
Lebohang Msase <msaselebohang@gmail.com>
2023-02-23 15:47:06 -0800

    Multiplying integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies integers together
+ *
+ * @a, @b: The ASCII code
+ *
+ * Return: Always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	/*scanf("%d %d", &a, &b);*/
+
+	prod  = a * b;
+	
+	_putchar(prod);
+
+	return (0);
+} 
