9c33d24fc65de036d95c3619927e5810f0689ca7
Lebohang Msase <msaselebohang@gmail.com>
2023-02-23 23:42:09 -0800

    printing most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints most numbers except 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+
+		if (i == 2 && i == 4)
+		{
+			continue;
+		}
+
+	}
+
+	_putchar('\n');
+
+}
