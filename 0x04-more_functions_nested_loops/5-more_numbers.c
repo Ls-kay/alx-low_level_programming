002faffddc0b3a66538209abdbf21b5ac27624a6
Lebohang Msase <msaselebohang@gmail.com>
2023-02-24 03:41:10 -0800

    Printing more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,20 @@
 void more_numbers(void)
 {
 	int i;
-        int j;
+	int j;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j > 9)
-                        {
-                            _putchar((j / 10) + '0');
-                        }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
 
-                        _putchar((j % 10) + '0');
+			_putchar((j % 10) + '0');
+		}
 
-                }
-                _putchar('\n');
-        }
+		_putchar('\n');
+	}
 }
