7cb602ed24e8a0d10500b16b8b219260fca778d0
Lebohang Msase <msaselebohang@gmail.com>
2023-02-24 04:01:11 -0800

    Drawing on the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - Draws a line in the terminal
+ *
+ * @n: The ASCII code
+ *
+ */
+
+void print_line(int n)
+{
+	char p = '_';
+
+	if (n > 0)
+	{
+		_putchar(p);
+		_putchar('\n');
+	}
+	else if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
