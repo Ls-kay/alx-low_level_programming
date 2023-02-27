defc55309886df61d5cfccfd2facb24f14581645
Lebohang Msase <msaselebohang@gmail.com>
2023-02-27 11:18:05 -0800

    Pointing to int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,12 @@
 /**
  * reset_to_98 - Points to an integer and updates it to 98
  *
- * @*n: The ASCII code 
+ * @n: The ASCII code
  *
  */
 
 void reset_to_98(int *n)
 {
-	int p = &n;
-
 	*n = 98;
 
-	_putchar(n);
 }
