f873bd96f304c309df3c92c75ca06cb937ffb092
Lebohang Msase <msaselebohang@gmail.com>
2023-02-28 00:20:38 -0800

    fixing errors

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts2 - Prints every other character
+ *
+ * @str: The string
+ *
+ */
+
+void puts2(char *str)
+{
+	/*int i;*/
+
+	/*for (i = 0; i < *str; i++)*/
+	/*{*/
+		puts(str[i]);
+	/*}*/
+}
