e99e84092ecc39dfbd1af82af1ce7a46af094b75
Lebohang Msase <msaselebohang@gmail.com>
2023-03-01 06:38:31 -0800

    Copying strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncat - Concatinates strings
+ *
+ * @dest: Destination string
+ *
+ * @src: Source string
+ *
+ * @n: Max number of characters appended
+ *
+ * Return: Destination string
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	puts(dest);
+
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncpy - Concatinates strings
+ *
+ * @dest: Destination string
+ *
+ * @src: Source string
+ *
+ * @n: Max number of characters appended
+ *
+ * Return: Destination string
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+
+	puts(dest);
+
+	return (0);
+}
